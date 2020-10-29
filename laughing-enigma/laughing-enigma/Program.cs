2f220e47118782afb0a4cb23c1a22915aa154496
Keith <keithtmccartney@hotmail.com>
2020-10-29 18:17:29 +0000

    Adding a (.NET Core) Console App.

---
+++ laughing-enigma/laughing-enigma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laughing_enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
